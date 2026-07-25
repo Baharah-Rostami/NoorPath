fb985396d178ff3fdfa1bf1191f45f7fbb248070
Baharah-Rostami <baharrustami888@gmail.com>
2026-07-25 11:32:10 +0430

    stats cards added to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import DashboardBanner from "@/components/dashboard/DashboardBanner";
+import StatsCards from "@/components/dashboard/StatsCard";
+
 export default function DashboardPage() {
   return (
    <main className="space-y-8">
 
     <DashboardBanner />
-
+    <StatsCards/>
 </main>
   );
 }
---
+++ components/dashboard/StatsCard.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import {
+  Briefcase,
+  Bookmark,
+  Globe,
+  TrendingUp,
+} from "lucide-react";
+
+import { getOpportunities } from "@/lib/opportunitiesStorage";
+import { getSavedIds } from "@/lib/savedOpportunities";
+
+import type { Opportunity } from "@/data/opportunities";
+
+
+export default function StatsCards() {
+
+  const [opportunities, setOpportunities] = useState<Opportunity[]>([]);
+  const [savedCount, setSavedCount] = useState(0);
+
+
+  useEffect(() => {
+
+    setOpportunities(
+      getOpportunities()
+    );
+
+
+    setSavedCount(
+      getSavedIds().length
+    );
+
+  }, []);
+
+
+
+  const stats = [
+    {
+      title: "Total Opportunities",
+      value: opportunities.length,
+      subtitle: "+2 today",
+      icon: Briefcase,
+    },
+
+    {
+      title: "Saved Opportunities",
+      value: savedCount,
+      subtitle: "Bookmarked",
+      icon: Bookmark,
+    },
+
+    {
+      title: "Remote Jobs",
+      value: opportunities.filter(
+        (job)=>job.remote
+      ).length,
+      subtitle: "Available",
+      icon: Globe,
+    },
+
+    {
+      title: "Featured Opportunities",
+      value: opportunities.filter(
+        (job)=>job.featured
+      ).length,
+      subtitle: "Trending",
+      icon: TrendingUp,
+    },
+  ];
+
+
+
+  return (
+    <section
+      className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6
+      ">
+
+      {
+        stats.map((stat)=>{
+
+          const Icon = stat.icon;
+
+
+          return (
+            <div
+              key={stat.title}
+              className=" bg-white dark:bg-zinc-900 rounded-2xl p-6 border border-gray-100 dark:border-zinc-800 shadow-sm hover:shadow-md  transition
+              "
+            >
+
+              <div className=" flex items-center justify-between
+              ">
+
+                <div>
+
+                  <p className="
+                  text-sm
+                  text-gray-500
+                  dark:text-gray-400
+                  ">
+                    {stat.title}
+                  </p>
+
+
+                  <h2 className="
+                  text-3xl
+                  font-bold
+                  mt-2
+                  text-gray-900
+                  dark:text-white
+                  ">
+                    {stat.value}
+                  </h2>
+
+
+                  <p className="
+                  text-xs
+                  text-green-500
+                  mt-2
+                  ">
+                    {stat.subtitle}
+                  </p>
+
+                </div>
+
+
+
+                <div className="
+                p-3
+                rounded-xl
+                bg-green-100
+                dark:bg-green-900/30
+                ">
+
+                  <Icon
+                    size={24}
+                    className="
+                    text-green-700
+                    dark:text-green-500
+                    "
+                  />
+
+                </div>
+
+              </div>
+
+            </div>
+          );
+
+        })
+      }
+
+
+    </section>
+  );
+}
